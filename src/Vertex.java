f872d232144afa8153a8d489e1feed93316a6798
Kazbek <k.ahmetov123@gmail.com>
2025-05-16 19:37:11 +0500

    Vertex created

---
+++ src/Vertex.java
@@ -0,0 +1,29 @@
+public class Vertex<T> {
+    private T data;
+
+    public Vertex(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return data.equals(vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
